83b22c097eec3a5db90e8889328de1913fe1d3eb
Christopher <christopher.carnivale@gmail.com>
2019-02-19 12:06:13 -0500

    1st exercise done

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('dev')); 
+
+app.get('/sum', (req, res) => {
+  const {a, b} = req.query;
+  const numA = Number(a);
+  const numB = Number(b); 
+  const c = numA + numB;
+
+  res.send(`The sum of ${a} and ${b} is ${c}`); 
+}); 
+
+app.listen(8000, () => {
+  console.log('Server listening on port 8000');
+});
